fcbad991fcc9f901be63c307dc5e0f942926a2fb
thierryH91200 <thierry.hentic@gmail.com>
2025-10-23 16:18:01 +0200

    Initial Commit

---
+++ Planifia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planifia
+//
+//  Created by thierryH24 on 23/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planifia/PlanifiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanifiaApp.swift
+//  Planifia
+//
+//  Created by thierryH24 on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanifiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
